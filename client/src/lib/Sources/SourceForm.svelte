cd7f92f80bc9ced9d11134e1603e4ee4da2a94cb
koplas <pschwabauer@intevation.de>
2024-09-05 16:38:33 +0200

    Use number input; don't use hardcoded default values

--- client/src/lib/Sources/SourceForm.svelte
+++ client/src/lib/Sources/SourceForm.svelte
@@ -17,7 +17,8 @@
     Checkbox,
     Fileupload,
     Input,
-    Label
+    Label,
+    NumberInput
   } from "flowbite-svelte";
   export let formClass: string = "";
   export let source: Source;
@@ -158,11 +159,11 @@
         </div>
         <div>
           <Label>Rate</Label>
-          <Input bind:value={source.rate}></Input>
+          <NumberInput placeholder={1} bind:value={source.rate}></NumberInput>
         </div>
         <div>
           <Label>Slots</Label>
-          <Input bind:value={source.slots}></Input>
+          <NumberInput placeholder={2} bind:value={source.slots}></NumberInput>
         </div>
       </div>
 
