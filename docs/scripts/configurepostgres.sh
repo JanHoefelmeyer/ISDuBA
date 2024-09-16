fdd78e94b61f4867dbd0204afdff4ad91dbf5412
JanHoefelmeyer <jan.hoefelmeyer@intevation.de>
2024-09-16 10:48:25 +0200

    Maybe without echo?

--- docs/scripts/configurepostgres.sh
+++ docs/scripts/configurepostgres.sh
@@ -19,7 +19,7 @@ whoami
 echo "HERE IS 'ls -l'"
 echo $(id; ls -l)
 echo "------------------_"
-echo $(sudo -u postgres --list)
+sudo -u postgres --list
 echo "END OF ECHO IN CONFIGUREPOSTGRES"
 
 # Alter PostgreSQL as postgres user
